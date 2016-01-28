3ae4f43c9c45efaea2ab18e91047422c9d3f37e5
Peramanathan <prem@weavler.it>
2016-01-28 17:43:13 +0100

    [Server] Simple refactored backend app that smoothly renders the frontend with demo login function

--- server.py
+++ server.py
@@ -11,21 +11,29 @@ Author: Peramanathan Sathyamoorthy (prem@weavler.it)
 :copyright: (c) 2016 by Weavler AB
 """
 
-from flask import Flask, render_template
-
-# By default flask looks `static` folder for assests
-# and `templates` folder for index and other html templates.
-# NB: Changing this default config will lead us to rewrite assests Path
-# in all the html templates with `url_for('static')` in all html files.
-# <script src="js/script.min.js"> should be written
-# <script src="{{ url_for('static', filename=js/script.min.js")}}"">
-app = Flask(__name__, static_folder='app', template_folder='app')
+from flask import Flask, render_template, request, redirect, url_for
 
 
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    """The landing page."""
-    return render_template('index.html')
+    """The landing page with user authentication."""
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] != 'admin' or \
+                request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('dashboard'))
+    return render_template('index.html', error=error)
+
+
+@app.route('/dashboard')
+def dashboard():
+    """Dashboard for the specific user as Single Page Application(SPA)."""
+    return render_template("dashboard.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
